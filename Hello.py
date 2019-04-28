7e4e77ea34ba64b91611cff65cea7344cfe06db9
Simranjeet Singh <48654626+smrnjeet222@users.noreply.github.com>
2019-04-28 16:05:41 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,70 @@
+print("Hello World")
+number = 300
+print(number) 
+number =40430
+print(number) 
+stringvar =  "send "
+stringvar2 = 'pics '
+stringvar3 = "quick \n"
+newstring = (stringvar + stringvar2 +stringvar3)*4
+print (stringvar + stringvar2 +stringvar3)
+print(newstring)
+
+number =  -5.03 + 3j
+number2 = -5.03
+number3 = number + number2 
+print(type(number))
+print(type(number2))
+print(type(number3))
+print (number3)
+
+stringvar4 = "5.0"
+number4 = 10
+number5 = float(stringvar4) + number4
+print(number5)
+
+stringvar4 = "ggg"
+number4 = 10
+stringvar5 = stringvar4 + str(number4)
+print(stringvar5)
+
+string = " bob the builder "
+print(string[3:14])
+print(len(string))
+print(string.strip())
+
+string = "shUt up"
+print(string.capitalize())
+print(string.upper())
+print(string.lower())
+print(string.replace('u', 'U'))
+print(string.replace('U', '7'))
+
+string = "shnshnshnsh"
+print(string.split('n'))
+
+
+
+
+msg = "Your Name:"
+print(msg)
+user_name = input()
+print ("Hello " + user_name)
+print(type(user_name))
+
+
+number = 10 ** 2 # **  power of
+print(number)
+number = 10/4
+print(number)
+number= 10//4 
+print(number)
+number *= 5
+print(number)
+
+
+
+
+
+
+input('             .....Programme ends.....')
---
+++ new_learn.py
@@ -0,0 +1,26 @@
+email = '''Hi Simranjeet Singh,
+             this is an email 
+                        to you for strings.''' 
+print (email) 
+print (email[0:13])
+
+print (email[:-5])
+print (email[3:-2])
+# if left [:] empty == [0:length_of_string]
+
+print('enter any number')
+rank = int(input())
+print (rank + 10)
+
+first = "Simranjeet"
+last = "Singh"
+msg = f'{last} {first} is a coder.'
+print(msg)  
+print('Singh' in msg)
+
+
+
+
+
+
+input('     ......Programme Ends.....    ')
